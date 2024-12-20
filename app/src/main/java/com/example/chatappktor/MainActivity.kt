65a591897fe6bd1ac954ca5ef0706f528cb1e121
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2024-12-20 23:50:20 +0530

    final commit

--- app/src/main/java/com/example/chatappktor/MainActivity.kt
+++ app/src/main/java/com/example/chatappktor/MainActivity.kt
@@ -11,16 +11,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.chatappktor.presentation.chat.ChatScreen
+import com.example.chatappktor.presentation.username.UsernameScreen
 import com.example.chatappktor.ui.theme.ChatAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ChatAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "username_screen") {
+                    composable("username_screen"){
+                        UsernameScreen(onNavigate = navController::navigate)
+                    }
+                    composable(
+                        "chat_screen/{username}",
+                        arguments = listOf(
+                            navArgument(name = "username"){
+                                type = NavType.StringType
+                                nullable = true
+                            }
+                        )
+                    ){
+                        val username = it.arguments?.getString("username")
+                        ChatScreen(username = username)
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/chatappktor/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/example/chatappktor/presentation/chat/ChatScreen.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +14,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -24,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -34,7 +41,7 @@ import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun ChatScreen(
-    username : String,
+    username : String?,
     viewModel: ChatViewModel = hiltViewModel()
 ) {
 
@@ -110,7 +117,10 @@ fun ChatScreen(
                                     color = if (isOwnMessage) Color.Green else Color.DarkGray
                                 )
                             }
-                            .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+                            .background(
+                                color = if (isOwnMessage) Color.Green else Color.DarkGray,
+                                shape = RoundedCornerShape(10.dp)
+                            )
                             .padding(8.dp)
                     ) {
                         Text(
@@ -130,8 +140,31 @@ fun ChatScreen(
                         )
                     }
                 }
+                Spacer(Modifier.height(25.dp))
             }
         }
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            TextField(
+                value = viewModel.messageText.value,
+                onValueChange = viewModel::onMessageChange,
+                placeholder = {
+                    Text(text = "message")
+                },
+                modifier = Modifier.weight(1f)
+
+            )
+            IconButton(onClick = viewModel::sendMessage) {
+                Icon(
+                    imageVector = Icons.Default.Send,
+                    contentDescription = "Send icon button to send msg"
+                )
+            }
+
+        }
+
     }
 }
 
